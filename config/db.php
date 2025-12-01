dcbc88dd5d21ee25a087e44a76780b0f37c7b7f8
Jayyy473 <jaden.jjoseph@gmail.com>
2025-12-01 14:49:54 -0400

    .

--- config/db.php
+++ config/db.php
@@ -1,15 +1,16 @@
 <?php
-$host = "mysql-273006ce-tamcc.k.aivencloud.com";
-$port = 17199;
-$user = "avnadmin";
-$pass = "AVNS_ttME4P816txlP2F5mVY";
-$dbname = "TaskHive";
-$ssl_ca = __DIR__ . '/../ca.pem'; 
+// Get Aiven connection settings from Render environment variables
+$host = getenv('Aiven_Host');
+$port = getenv('Aiven_Port');
+$username = getenv('Aiven_User');
+$password = getenv('Aiven_Pass');
+$dbname = getenv('Aiven_DB');
+$ssl_ca = __DIR__ . '/ca.pem'; 
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
 
-if (!mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+if (!mysqli_real_connect($conn, $host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
